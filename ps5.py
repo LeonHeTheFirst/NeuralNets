9c9d1ad52595b5f531f2c14e34a665d53ab6b9a0
Leon He <Leon.He@jhuapl.edu>
2017-10-05 19:33:16 -0400

    ps5 methods

--- ps5.py
+++ ps5.py
@@ -10,18 +10,18 @@ def method1():
 	layer_list.append(PerceptronLayer(perceptron_list=first_layer, output_flag=False))
 	layer_list.append(PerceptronLayer(perceptron_list=second_layer, output_flag=True))
 	test_net = PerceptronNet(layer_list)
-	inputs1 = [1,2]
-	desired1 = [0.7]
-	inputs2 = [1,2]
-	desired2 = [0.7]
+	inputs1 = [1,1]
+	desired1 = [0.9]
+	inputs2 = [-1,-1]
+	desired2 = [0.05]
 
 	logging.debug(test_net.output_vector(inputs1))
 	logging.debug(test_net.output_deltas(inputs1, desired1))
 
 	for i in range(15):
 		test_net.train(inputs1, desired1)
-	for i in range(15):
 		test_net.train(inputs2, desired2)
+
 
 def method2():
 	first_layer = []
@@ -33,17 +33,19 @@ def method2():
 	layer_list.append(PerceptronLayer(perceptron_list=first_layer, output_flag=False))
 	layer_list.append(PerceptronLayer(perceptron_list=second_layer, output_flag=True))
 	test_net = PerceptronNet(layer_list)
-	inputs1 = [1,2]
-	desired1 = [0.7]
-	inputs2 = [1,2]
-	desired2 = [0.7]
+	inputs1 = [1,1]
+	desired1 = [0.9]
+	inputs2 = [-1,-1]
+	desired2 = [0.05]
 
 	logging.debug(test_net.output_vector(inputs1))
 	logging.debug(test_net.output_deltas(inputs1, desired1))
 
 	for i in range(15):
 		test_net.train(inputs1, desired1)
+	for i in range(15):
 		test_net.train(inputs2, desired2)
+
 
 if __name__ == '__main__':
 	method1()
