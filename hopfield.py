9dc522d0b3879ba914abe12c26f0515f94bca025
Leon He <Leon.He@jhuapl.edu>
2017-11-02 20:37:03 -0400

    finished network

--- hopfield.py
+++ hopfield.py
@@ -9,17 +9,29 @@ logger = logging.getLogger(__name__)
 
 class HopfieldNet():
 	def __init__(self, size):
-		self.matrix = 0
+		self.matrix = np.asmatrix(np.zeros(shape = (size, size)))
 
 	def layer_count(self):
 		pass
 
-	def input_vector_size():
-		return self.matrix.x
+	def get_size(self):
+		return self.matrix.shape[0]
 
 	def output_vector(self, input_vector):
-		output = self.matrix * input_vector
-		return output
+		history = []
+		next_output = np.asmatrix(input_vector).transpose()
+		print('input: ', next_output)
+		while not any((next_output == mat).all() for mat in history[:-1]):
+			next_output = np.matmul(self.matrix, next_output)
+			for i, value in np.ndenumerate(next_output):
+				if value > 1:
+					next_output[i] = 1
+				elif value < -1:
+					next_output[i] = -1
+			history.append(next_output.copy())
+			print(next_output)
+		print(next_output)
+		return next_output.transpose()
 
 	def error_vector(self, input_vector, desired_output_vector):
 		pass
@@ -28,7 +40,9 @@ class HopfieldNet():
 		pass
 
 	def train(self, exemplar):
-		pass
+		"""exemplar must be np.array()"""
+		new = np.matmul(np.asmatrix(exemplar).transpose(), np.asmatrix(exemplar)) - np.identity(self.get_size())
+		self.matrix += new
 
 	def big_e(self, input_vector, desired_output_vector):
 		pass
@@ -37,8 +51,12 @@ class HopfieldNet():
 		return self.matrix
 
 def test_network():
-	pass
+	testnet = HopfieldNet(9)
+	for i in range(10):
+		testnet.train([1,1,1,1,1,1,1,1,1])
+	print(testnet.matrix)
+	testnet.output_vector([-1,1,1,-1,1,-1,1,1,-1])
 
 
 if __name__ == '__main__':
-	pass
+	test_network()
